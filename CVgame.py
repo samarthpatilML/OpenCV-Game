4885888d5e8707a4b3e5a1346fc1a4681dc8f792
Samarth Patil <102871567+samarthpatilML@users.noreply.github.com>
2024-12-01 19:45:53 +0530

    update

--- CVgame.py
+++ CVgame.py
@@ -41,7 +41,56 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
         font=cv2.FONT_HERSHEY_SIMPLEX
         color=(255,0,255)
         text=cv2.putText(image,"Score",(480,30),font,1,color,4,cv2.LINE_AA)
-        text=cv2.putText(image,str(score),(590,30)                )
+        text=cv2.putText(image,str(score),(590,30) ,font,1,color,4,cv2.LINE_AA)
+        
+        
+        enemy() 
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
+                                          mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2,circle_radius=2),
+                )
+                
+                
+        if results.multi_hand_landmarks !=None:
+            for hand in results.multi_hand_landmarks:
+                for point in mp_hands.HandLandmark:
+                    
+                    
+                    normalizedLandmark = handLandmark.landmark[point]
+                    pixelCoordinatesLandmark = mp_drawing._normalized_to_pixel_coordinates(normalizedLandmark.x,normalizedLandmark.y, imageWidth,imageHeight)
+                    
+                    point=str(point)
+                    if point=="mp_hands.HandLandmark.INDEX_FINGER_TIP":
+                        try:
+                            cv2.circle(image, (pixelCoordinatesLandmark[0], pixelCoordinatesLandmark[1]), 25,(0,200,0),5)
+                            if pixelCoordinatesLandmark[0]==x_enemy or pixelCoordinatesLandmark[0]==x_enemy+10 or pixelCoordinatesLandmark[0]==x_enemy-10 or pixelCoordinatesLandmark[0]==x_enemy-10:
+                                print("found")
+                                x_enemy=random.randint(50,600)
+                                y_enemy=random.randint(50,400)
+                                score=score+1
+                                font=cv2.FONT_HERSHEY_SIMPLEX
+                                color=(255,0,255)
+                                text=cv2.putText(frame,"Score",(100,100),font,1,color,4,cv2.LINE_AA)
+                                enemy()
+                                
+                        except:
+                            pass
+                        
+        cv2.imshow("Hand Tracking", image)
+        
+        
+        if cv2.waitKey(10) & 0xFF == ord("q"):
+            print(score)
+            break
+
+
+video.release()
+cv2.destroyALLWindows()                             
+                
+        
+        
+        
         
         
         
